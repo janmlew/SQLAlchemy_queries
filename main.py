9a2d5095d1b50ec9f75fa2628df56c3fb1744468
janmlew <janmikolajlewinski@gmail.com>
2024-04-22 14:42:00 +0200

    Added code for db creation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Imports
 import psycopg2
 from sqlalchemy import create_engine
+from sqlalchemy-utils import database_exists, create_database
 
 # Engine setup
 engine = create_engine("postgresql://postgres@/postgres")
